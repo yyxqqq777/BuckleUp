03743808ed28013938acb60c32114fc48315ea55
yyxqqq777 <yunxuan517@gmail.com>
2022-10-25 10:22:42 -0400

    Initial Commit

---
+++ WeatherPacker/WeatherPackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPackerApp.swift
+//  WeatherPacker
+//
+//  Created by Yunxuan Yu on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
