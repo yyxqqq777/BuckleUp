e2888f648cb6c1583623ce42df56ac0ca8932f1e
yyxqqq777 <yunxuan517@gmail.com>
2022-10-25 20:25:06 -0400

    build(WeatherPackerApp): add firebase config code

--- WeatherPacker/WeatherPackerApp.swift
+++ WeatherPacker/WeatherPackerApp.swift
@@ -3,15 +3,28 @@
 //  WeatherPacker
 //
 //  Created by Yunxuan Yu on 10/25/22.
-//
 
 import SwiftUI
+import Firebase
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct WeatherPackerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
