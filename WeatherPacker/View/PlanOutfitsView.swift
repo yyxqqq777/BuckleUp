8f09f657d324cc427855c82ab13bbe99c0e2886b
yyxqqq777 <yunxuan517@gmail.com>
2022-11-08 21:59:30 -0500

    feat(dailyOutfit): add a simple view

---
+++ WeatherPacker/View/PlanOutfitsView.swift
@@ -0,0 +1,24 @@
+//
+//  PlanOutfitsView.swift
+//  WeatherPacker
+//
+//  Created by Yunxuan Yu on 11/8/22.
+//
+
+import SwiftUI
+
+struct PlanOutfitsView: View {
+    var body: some View {
+      VStack {
+        HStack{
+          Rectangle()
+        }
+      }
+    }
+}
+
+struct PlanOutfitsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanOutfitsView()
+    }
+}
