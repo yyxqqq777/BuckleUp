df23a994cf220465d01e0f2c0626af1a59164973
yyxqqq777 <yunxuan517@gmail.com>
2022-11-10 20:13:13 -0500

    fix(trip): add padding for trip view

--- WeatherPacker/View/TripRowView.swift
+++ WeatherPacker/View/TripRowView.swift
@@ -8,36 +8,35 @@
 import SwiftUI
 
 struct TripRowView: View {
-    var trip: Trip
-    @EnvironmentObject var listBg: ListBackground
-    @State var dailyOutfitCollectionRepo = DailyOutfitCollectionRepository()
-    
-   
+  var trip: Trip
+  @EnvironmentObject var listBg: ListBackground
+  @State var dailyOutfitCollectionRepo = DailyOutfitCollectionRepository()
+  
+  
   var body: some View {
     NavigationLink(destination: DailyOutfitCollectionView(tripLocation: trip.tripLocation, tripId: trip.id)) {
-      VStack {
-        VStack {
+      ZStack {
+        Image("ListBG_\(listBg.getIndex())")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 350, height: 200, alignment: .center)
+          .clipShape(RoundedRectangle(cornerRadius: 16))
+          .overlay(RoundedRectangle(cornerRadius: 16)
+            .fill(Color.black.opacity(0.15)).frame(width: 350, height: 200))
+        VStack (alignment: .leading) {
+          Text("\(trip.tripLocation)")
+            .font(.title)
+            .bold()
+            .foregroundColor(.white)
+            .padding(EdgeInsets(top: 32, leading: -80, bottom: 0, trailing: 0))
           Spacer()
-          HStack {
-            Text("\(trip.tripLocation)")
-              .font(.title2)
-              .bold()
-              .foregroundColor(.white)
-            Spacer()
-          }
-          Spacer()
-          HStack {
-            Text("\(trip.tripStartDate) - \(trip.tripEndDate)")
-              .foregroundColor(.white)
-              .bold()
-            Spacer()
-          }
-          Spacer()
+          Text("\(trip.tripStartDate) - \(trip.tripEndDate)")
+            .foregroundColor(.white)
+            .bold()
+            .padding(EdgeInsets(top: 0, leading: -80, bottom: 32, trailing: 0))
         }
       }
+      
     }
-    .frame(height: 130)
-    .listRowBackground(Image("ListBG_\(listBg.getIndex())"))
   }
-
 }
